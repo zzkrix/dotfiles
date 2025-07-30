0f584d03134dc6d921497509d5f50c709f14b636
zzkrix <zzkrix56@gmail.com>
2025-07-30 13:02:08 +0800

    配置neo-tree一直显示隐藏文件

---
+++ nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,13 @@
+return {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+        filesystem = {
+            filtered_items = {
+                visible = true, -- 显示隐藏文件
+                -- hide_dotfiles = false,
+                -- show_hidden_count = true,
+                -- hide_gitignored = false,
+            },
+        },
+    },
+}
