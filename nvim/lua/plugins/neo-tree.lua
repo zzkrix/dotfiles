d79bc4fb0bd0f9413d5bb697edfb9c2d58793fef
zzkrix <zzkrix56@gmail.com>
2025-08-02 09:11:03 +0800

    nvim: config neo-tree auto update file stats

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,13 +1,27 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    opts = {
-        filesystem = {
-            filtered_items = {
-                visible = true, -- 显示隐藏文件
-                -- hide_dotfiles = false,
-                -- show_hidden_count = true,
-                -- hide_gitignored = false,
-            },
-        },
-    },
+	"nvim-neo-tree/neo-tree.nvim",
+	opts = {
+		filesystem = {
+			filtered_items = {
+				visible = true, -- 显示隐藏文件
+				-- hide_dotfiles = false,
+				-- show_hidden_count = true,
+				-- hide_gitignored = false,
+			},
+		},
+		event_handlers = {
+			-- {
+			--     event = "file_open_requested",
+			--     handler = function()
+			--         require("neo-tree.command").execute({ action = "close" })
+			--     end,
+			-- },
+			{
+				event = "git_status_changed", -- 监听 Git 状态变化
+				handler = function()
+					require("neo-tree.sources.manager").refresh("filesystem")
+				end,
+			},
+		},
+	},
 }
