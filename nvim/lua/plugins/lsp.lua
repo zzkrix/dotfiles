0c3ee3b431bcba3003840d4f1a0803b06dba65f3
zzkrix <zzkrix56@gmail.com>
2025-08-02 09:48:40 +0800

    close golang inlay-hints

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,22 +1,33 @@
 return {
-    "neovim/nvim-lspconfig",
-    opts = {
-        -- inlay_hints = { enabled = false },
-        servers = {
-            gopls = {
-                settings = {
-                    gopls = {
-                        -- goimports = true,
-                        -- gofumpt = true,
-                        analyses = {
-                            unusedparams = false,
-                            ST1000 = false,
-                            -- ST1003 = false,
-                            -- ST1012 = false,
-                        },
-                    },
-                },
-            },
-        },
-    },
+	"neovim/nvim-lspconfig",
+	opts = {
+		-- inlay_hints = { enabled = false },
+		servers = {
+			gopls = {
+				settings = {
+					gopls = {
+						-- goimports = true,
+						-- gofumpt = true,
+						analyses = {
+							unusedparams = false,
+							ST1000 = false,
+							-- ST1003 = false,
+							-- ST1012 = false,
+						},
+
+						-- 直接在 gopls 设置中关闭 inlay hints
+						hints = {
+							assignVariableTypes = false,
+							compositeLiteralFields = false,
+							compositeLiteralTypes = false,
+							constantValues = false,
+							functionTypeParameters = false,
+							parameterNames = false,
+							rangeVariableTypes = false,
+						},
+					},
+				},
+			},
+		},
+	},
 }
