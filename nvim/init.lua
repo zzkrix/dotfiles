0803c351cd57147660328ad29f8b197d8a2624b3
zzkrix <zzkrix56@gmail.com>
2025-08-02 13:58:14 +0800

    nvim: 设置文件管理器被选中行的背景色

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- 设置neo-tree的被选中行背景色
+-- vim.api.nvim_set_hl(0, "NeoTreeCursorLine", { bg = "gray", fg = "white" })
+-- vim.api.nvim_set_hl(0, "NeoTreeCursorLine", { bg = "#264F77", fg = "#ffffff" })
+vim.api.nvim_set_hl(0, "NeoTreeCursorLine", { bg = "#2E3A46", fg = "#ffffff" })
--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -10,12 +10,22 @@ return {
 			},
 		},
 		event_handlers = {
+			-- 显示相对行号
 			-- {
-			--     event = "file_open_requested",
-			--     handler = function()
-			--         require("neo-tree.command").execute({ action = "close" })
-			--     end,
+			-- 	event = "neo_tree_buffer_enter",
+			-- 	handler = function()
+			-- 		vim.opt_local.relativenumber = true
+			-- 	end,
 			-- },
+
+			-- -- 当文件被打开时，自动关闭文件管理器
+			-- {
+			-- 	event = "file_open_requested",
+			-- 	handler = function()
+			-- 		require("neo-tree.command").execute({ action = "close" })
+			-- 	end,
+			-- },
+
 			{
 				event = "git_status_changed", -- 监听 Git 状态变化
 				handler = function()
