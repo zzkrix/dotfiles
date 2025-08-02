d06129fcac6d8631646b71fb18459501e7fa8fec
zzkrix <zzkrix56@gmail.com>
2025-08-02 22:19:46 +0800

    nvim: terminal 和 lazygit 设置为透明

--- nvim/lua/plugins/terminal.lua
+++ nvim/lua/plugins/terminal.lua
@@ -1,29 +1,34 @@
 return {
-    "folke/snacks.nvim",
-    ---@type snacks.Config
-    opts = {
-        terminal = {
-            win = {
-                -- "float"|"bottom"|"top"|"left"|"right"
-                position = "float",
+	"folke/snacks.nvim",
+	---@type snacks.Config
+	opts = {
+		terminal = {
+			win = {
+				-- "float"|"bottom"|"top"|"left"|"right"
+				position = "float",
 
-                -- rounded 圆角
-                -- single 直角单条边
-                -- double 直角两条边
-                border = "rounded", -- 边框样式
+				-- rounded 圆角
+				-- single 直角单条边
+				-- double 直角两条边
+				border = "rounded", -- 边框样式
 
-                height = 0.95,
-                width = 0.95,
-            },
-        },
-    },
-    keys = {
-        {
-            "<c-/>",
-            function()
-                require("snacks").terminal.toggle()
-            end,
-            desc = "Toggle Floating Terminal",
-        },
-    },
+				height = 0.85,
+				width = 0.85,
+
+				wo = {
+					-- winblend = 15,
+					winhighlight = "Normal:SnacksTerminalNormal,FloatBorder:SnacksTerminalBorder",
+				},
+			},
+		},
+	},
+	keys = {
+		{
+			"<c-/>",
+			function()
+				require("snacks").terminal.toggle()
+			end,
+			desc = "Toggle Floating Terminal",
+		},
+	},
 }
