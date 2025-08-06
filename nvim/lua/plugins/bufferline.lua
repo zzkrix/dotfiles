d09059c099685f1a0d63e3b305f9df2796e246f8
zzkrix <zzkrix56@gmail.com>
2025-08-06 10:54:30 +0800

    nvim: update bufferline

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -4,16 +4,35 @@ return {
 		opts = {
 			options = {
 				always_show_bufferline = true,
-				-- separator_style = "thick",
+				show_buffer_close_icons = false,
+
+				-- tabs模式下，只显示一个buffer标签
+				-- mode = "buffers", -- set to "tabs" to only show tabpages instead
+				indicator = {
+					-- icon = '|', -- this should be omitted if indicator style is not 'icon'
+					style = 'none', --'icon' | 'underline' | 'none',
+				},
+
+				-- indicator = {
+				-- 	icon = '🏷️', -- this should be omitted if indicator style is not 'icon'
+				-- 	style = 'icon', --'icon' | 'underline' | 'none',
+				-- },
+
+				max_name_length = 30, -- 每个 buffer 名称最大显示长度
+				max_prefix_length = 15, -- 如果有相同文件名时的前缀显示长度
+				tab_size = 0, -- buffer 宽度自适应
+
+				-- separator_style = "slant" | "slope" | "thick" | "thin" | { 'any', 'any' },
+				separator_style = { '', '' },
+
+				-- 配置文件管理器上方位置
 				offsets = {
 					{
 						filetype = "neo-tree",
 						text = "Neo-tree",
 						highlight = "Directory",
 						text_align = "left",
-					},
-					{
-						filetype = "snacks_layout_box",
+						separator = "",
 					},
 				},
 			},
