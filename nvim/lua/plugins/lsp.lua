57248f3227dcc804fe705712498cfff6d7dae1c4
zzkrix <zzkrix56@gmail.com>
2025-08-10 10:25:37 +0800

    nvim: 给lsp自动完成弹框加上边框

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,35 +1,53 @@
 return {
-	"neovim/nvim-lspconfig",
-	opts = {
-		-- inlay_hints = { enabled = false },
-		servers = {
-			gopls = {
-				settings = {
-					gopls = {
-						-- goimports = true,
-						-- gofumpt = true,
-						analyses = {
-							unusedparams = false,
-							ST1000 = false,
-							ST1020 = false,
-							ST1022 = false,
-							-- ST1003 = false,
-							-- ST1012 = false,
-						},
+	{
+		"neovim/nvim-lspconfig",
 
-						-- 直接在 gopls 设置中关闭 inlay hints
-						hints = {
-							assignVariableTypes = false,
-							compositeLiteralFields = false,
-							compositeLiteralTypes = false,
-							constantValues = false,
-							functionTypeParameters = false,
-							parameterNames = false,
-							rangeVariableTypes = false,
+		dependencies = {
+			{
+				"saghen/blink.cmp",
+				opts = {
+					-- 给提示信息加边框
+					completion = {
+						menu = {
+							-- 可选值：nil, 'none', 'single', 'double', 'rounded', 'solid', 'shadow'
+							border = 'rounded',
+						},
+						documentation = {
+							window = {
+								border = "rounded",
+							},
+						},
+					},
+					signature = {
+						window = {
+							border = "rounded",
+						},
+					},
+				},
+			}
+		},
+
+		opts = {
+			inlay_hints = {
+				enabled = true, -- 全局启用
+				exclude = { "go" }, -- 排除这些文件类型
+			},
+
+			servers = {
+				gopls = {
+					settings = {
+						gopls = {
+							analyses = {
+								unusedparams = false,
+								ST1000 = false,
+								ST1020 = false,
+								ST1021 = false,
+								ST1022 = false,
+							},
 						},
 					},
 				},
 			},
 		},
-	},
+	}
 }
